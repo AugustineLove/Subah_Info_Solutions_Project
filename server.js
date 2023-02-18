e84585fed314f1bdeeda243fc88b9466d88b65f3
AugustineLove <106684513+AugustineLove@users.noreply.github.com>
2023-02-18 18:05:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const DB_URL = "mongodb+srv://AugustineLove:stephens03@cluster0.1qgu7j2.mongodb.
 const User = require('./model/studentModel');
 const bodyParser = require('body-parser')
 const server = express()
-const PORT = process.env.PORT || 2000
+const port = process.env.PORT || 2000
 
 
 server.use(bodyParser.json());
@@ -43,7 +43,7 @@ server.post("/add", async (req, res) => {
 });
 
 //Running application at localhost:2000
-server.listen(PORT, function (){
+server.listen(port, function (){
     console.log("Server is running on 2000")
     mongoose.connect(DB_URL).then(
         function(){
